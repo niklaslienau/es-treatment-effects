7d835fdb515150b32cae441f57815645fef59fa0
niklaslienau <nikilienau@gmail.com>
2025-08-29 10:59:30 +0200

    Random Coefficient DGP with closed form CTATE and QTE solutions

--- dgp.R
+++ dgp.R
@@ -1,5 +1,4 @@
 
-
 # Load libraries
 library(MASS)
 library(ggplot2)
@@ -7,69 +6,14 @@ library(quantreg)
 library(quadprog)
 library(esreg)
 library(dplyr)
-#----------
-simulate_dgp_lin <- function(n = 1000, beta = 1, gamma = 0.5, pi = 1, rho = 0.7, cont = TRUE,  error_var= 1) {
-  # Simulate instrument
-  Z <- runif(n, min=0, max=2)
-  
-  # Simulate correlated errors (u, e)
-  Sigma <- matrix(c(1, rho, rho, 1), nrow = 2)
-  errors <- MASS::mvrnorm(n = n, mu = c(0, 0), Sigma = Sigma)
-  u <- errors[, 1]
-  e <- errors[, 2]
-  
-  # Generate endogenous treatment
-  D_latent <- pi * Z + e
-  
-  # Handle discrete vs continuous treatment
-  if (!cont) {
-    thresholds <- quantile(D_latent, probs = seq(0, 1, length.out = 17))
-    D <- cut(D_latent, breaks = thresholds, labels = 0:15, include.lowest = TRUE)
-    D <- as.numeric(as.character(D))
-  } else {
-    D <- D_latent
-  }
-  
-  # Additional independent noise
-  epsilon <- rnorm(n, mean = 0, sd = error_var)
-  
-  #structural error
-  v =  (gamma * abs(D)) * u + epsilon
-  
-  # Linear location scale: sigma(D) = gamma * D
-  Y <- beta * D + v
-  
-    
-  return(data.frame(Y = Y, D = D, Z = Z, u = u, e = e, v=v, epsilon=epsilon ))
-}
-##### no hetero  Option #####
-sim_dgp_homo = function(n= 1000, beta =1 , pi=1, rho=0.5){
-  
-  # Simulate correlated errors (u, e)
-  Sigma <- matrix(c(1, rho, rho, 1), nrow = 2)
-  errors <- MASS::mvrnorm(n = n, mu = c(0, 0), Sigma = Sigma)
-  u <- errors[, 1]
-  e <- errors[, 2]
-  
-  #Simulate Z from a runiform dist U(0,2)
-  Z= runif(n, 0,2)
-  
-  #Generate D
-  D = pi*Z + e
-  #Generate Y
-  Y= beta*D + u
-  
-  return(data.frame(Y = Y, D = D, Z = Z, u = u, e = e ))
-  
-}
+library(np)
 
-#-----------
 
 
 #### random coefficient model #######
 
 #sim data
-simulate_dgp = function(n=1000, pi=1, rho=0.5){
+simulate_dgp_rand = function(n=1000, pi=1, rho=0.5){
   ## DGP
   # Simulate correlated errors (u, e)
   Sigma <- matrix(c(1, rho, rho, 1), nrow = 2)
@@ -95,24 +39,85 @@ simulate_dgp = function(n=1000, pi=1, rho=0.5){
   ## True Quantile Effects
   
 }
-#calc true qte (analytical closed form not available)
-compute_qte_tau <- function(tau, pi = 1, n = 100000) {
-  # Draw D ~ N(0, pi^2 + 1)
-  D <- rnorm(n, mean = 1, sd = sqrt(pi^2 + 1))
+
+#compute true qte 
+compute_qte_tau_<- function(tau, pi = 1) {
+  g <- function(d) (d / sqrt(d^2 + 1)) * dnorm(d, mean = 1, sd = sqrt(pi^2 + 1))
+  ex <- integrate(g, lower = -Inf, upper = Inf, rel.tol = 1e-9)$value
   
   # Compute QTE(tau, d) pointwise
-  QTE_d <- D / sqrt(D^2 + 1)
+  qte_tau <- qnorm(tau) * ex
   
-  # Multiply by Phi^-1(tau) to get QTE(tau)
-  qte_tau <- qnorm(tau) * mean(QTE_d)
-  
+
   return(qte_tau)
 }
 
-
 #Plot true QTES
-taus <- seq(0.01, 0.99, by = 0.01)
+taus <- seq(0.001, 0.999, by = 0.01)
 qtes <- sapply(taus, compute_qte_tau)
 plot(taus, qtes, type = "l", main = "True QTE(τ)", xlab = "τ", ylab = "QTE(τ)")
+
+#compute the true CTATE (closed form available, could also integrate over QTE's)
+
+
+ compute_ctate_tau<- function(tau, pi = 1) {
+   z <- qnorm(tau)
+   g <- function(d) (d / sqrt(d^2 + 1)) * dnorm(d, mean = 1, sd = sqrt(pi^2 + 1))
+   ex <- integrate(g, lower = -Inf, upper = Inf, rel.tol = 1e-9)$value
+   -(dnorm(z)/tau) * ex
+ }
+ taus <- seq(0.001, 0.999, by = 0.01)
+ qtes <- sapply(taus, compute_ctate_tau)
+ plot(taus, qtes, type = "l", main = "True CTATE(τ)", xlab = "τ", ylab = "CTATE(τ)")
+
+ 
+########################################################################################
+ #---------- Location Scale
+ #sim data
+ simulate_dgp_locscale <- function(n = 1000, beta = 1, gamma = 0.5, pi = 1, rho = 0.5) {
+   # Simulate instrument
+   Z <- runif(n, min=0, max=2)
+   
+   # Simulate correlated errors (u, e)
+   Sigma <- matrix(c(1, rho, rho, 1), nrow = 2)
+   errors <- MASS::mvrnorm(n = n, mu = c(0, 0), Sigma = Sigma)
+   u <- errors[, 1]
+   e <- errors[, 2]
+   
+   # Generate endogenous treatment
+   D <- pi * Z + e
+   
+   
+   # Additional independent noise
+   # epsilon <- rnorm(n, mean = 0, sd = error_var)
+   
+   #structural error
+   v =  (gamma * abs(D)) * u 
+   
+   # Linear location scale: sigma(D) = gamma * D
+   Y <- beta * D + v
+   
+   
+   return(data.frame(Y = Y, D = D, Z = Z, u = u, e = e, v=v ))
+ }
+ 
+ 
+ #compute true qte
+ comp_qte_locscale = function(tau, beta=1, gamma=0.5){
+   qte = beta + gamma* qnorm(tau)
+   return(qte)
+ }
+ #Plot true QTES
+ taus <- seq(0.01, 0.99, by = 0.01)
+ qtes <- sapply(taus, comp_qte_locscale)
+ plot(taus, qtes, type = "l", main = "True QTE(τ)", xlab = "τ", ylab = "QTE(τ)")
+ 
+ 
+
+
+
+
+
+
 
 
